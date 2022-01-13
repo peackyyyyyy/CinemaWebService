ccf59322969e4f39bfa7d79155405877ab024fcf
hugo <hugo.malamaire06800@gmail.com>
2022-01-13 17:52:55 +0100

    add API view

--- src/main/java/com/example/cinemawebservice/film/FilmController.java
+++ src/main/java/com/example/cinemawebservice/film/FilmController.java
@@ -2,7 +2,9 @@ package com.example.cinemawebservice.film;
 
 import business.Film;
 import lombok.AllArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,17 +12,35 @@ import java.util.Optional;
 @RestController
 @AllArgsConstructor
 @CrossOrigin(origins = "*")
-@RequestMapping(path = "films")
 public class FilmController {
     private final FilmService filmService;
 
+    @RequestMapping(path = "api/films")
     @GetMapping()
     public List<Film> fetchAllFilms(@RequestParam(required = false) List<String> title){
         return filmService.get_films_with_params(Optional.ofNullable(title));
     }
 
+    @RequestMapping(path = "films")
+    @GetMapping()
+    public ModelAndView fetchFilmstemplate(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("films");
+        return modelAndView;
+    }
 
-    @RequestMapping(path = "/id={id}")
+    @RequestMapping(path = "films/id={id}")
+    @ModelAttribute
+    @GetMapping()
+    public ModelAndView fetchFilmtemplate(@PathVariable(value="id") String id, Model model){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("film");
+        model.addAttribute("id", id);
+        return modelAndView;
+    }
+
+
+    @RequestMapping(path = "api/films/id={id}")
     @GetMapping()
     public Optional<Film> fetchFilmbyId(@PathVariable(value="id") String id){
         return filmService.get_film_by_id(id);
